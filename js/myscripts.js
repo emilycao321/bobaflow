d65593da919f2fef02aa6e3a5b439668e4590a0a
Emily <emilycao@umich.edu>
2021-12-02 11:03:04 -0500

    new 11/2 morning js

--- js/myscripts.js
+++ js/myscripts.js
@@ -12,3 +12,20 @@ function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
  }
+
+//  const clear = (() => {
+//     const defined = v => v !== null && v !== undefined;
+//     const timeout = setInterval(() => {
+//         const ad = [...document.querySelectorAll('.ad-showing')][0];
+//         if (defined(ad)) {
+//             const video = document.querySelector('iframe');
+//             if (defined(video)) {
+//                 video.currentTime = video.duration;
+//             }
+//         }
+//     }, 500);
+//     return function() {
+//         clearTimeout(timeout);
+//     }
+// })();
+
